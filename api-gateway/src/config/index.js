c5054d05f94f3fb0d0c88144239285d309a2242b
sreehari <sreeharii859@gmail.com>
2026-06-25 23:03:50 +0530

    "corse middleware added"

--- api-gateway/src/config/index.js
+++ api-gateway/src/config/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || 'your_secret_here',
   JWT_INNER_ACCESS_SECRET: process.env.JWT_INNER_ACCESS_SECRET || 'inner_secret_here',
+  serviceTimeout: process.env.serviceTimeout || 5000,
+   REDIS_URL: process.env.REDIS_URL,
+   ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || 'http://localhost:3000',
 }
