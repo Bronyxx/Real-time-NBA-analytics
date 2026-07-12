89ba0acccc515c4d5723b762c6811b017ee64850
sreehari <sreeharii859@gmail.com>
2026-07-12 19:09:36 +0530

    bug while signing up

--- Player-service/src/index.js
+++ Player-service/src/index.js
@@ -1,22 +1,33 @@
+
 const express= require("express")
+require('dotenv').config();
 const helmet = require("helmet")
 const logger=require("./config/logger")
 const bcrypt = require('bcrypt');
+const authRoute= require("./routes/authRoutes")
 
 const app= express()
 app.use(helmet())
+
+app.use(express.json())              
+app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
   logger.http(`${req.method} ${req.url}`);
   next();
 });
+app.use('/api',authRoute)
 app.get("/",(req,res)=>{
     res.send("you are now in player service")
 })
 
 app.use((err, req, res, next) => {
   logger.error(err.message);
-  res.status(500).send('Internal Server Error');
+   const statusCode = err.statusCode || 500;
+        res.status(statusCode).json({
+    success: false,
+    message: err.isOperational ? err.message : 'Internal Server Error'
+  });
 });
 
 app.listen(3001,()=>{
